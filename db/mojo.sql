9e650a8e371b305c27c496e3c0b3e1bb164eaa0e
James Marchant <jamesm@afrihost.com>
2013-03-30 22:20:34 +0200

    MySQL structure updated to use Unix time

--- db/mojo.sql
+++ db/mojo.sql
@@ -1,66 +1,90 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 30, 2013 at 09:15 PM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+02:00";
 
 
--- -----------------------------------------------------
--- Table `mojo`.`client_details`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mojo`.`client_details` (
-  `client_id` INT NOT NULL AUTO_INCREMENT ,
-  `first_name` VARCHAR(255) NOT NULL ,
-  `last_name` VARCHAR(255) NOT NULL ,
-  `username` VARCHAR(255) NOT NULL ,
-  `created_time` DATETIME NOT NULL ,
-  `updated_time` DATETIME NOT NULL ,
-  PRIMARY KEY (`client_id`) ,
-  UNIQUE INDEX `client_id_UNIQUE` (`client_id` ASC) )
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
+--
+-- Database: `mojo`
+--
 
--- -----------------------------------------------------
--- Table `mojo`.`product_details`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mojo`.`product_details` (
-  `product_id` INT NOT NULL AUTO_INCREMENT ,
-  `product_name` VARCHAR(255) NOT NULL ,
-  `product_group` VARCHAR(255) NOT NULL ,
-  `product_vendor` VARCHAR(255) NOT NULL ,
-  `created_time` DATETIME NOT NULL ,
-  `updated_time` DATETIME NOT NULL ,client_details
-  PRIMARY KEY (`product_id`) )
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `client_details`
+--
 
--- -----------------------------------------------------
--- Table `mojo`.`client_product`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mojo`.`client_product` (
-  `client_product_id` INT NOT NULL AUTO_INCREMENT ,
-  `client_id` INT NOT NULL ,
-  `product_id` INT NOT NULL ,
-  `product_name` VARCHAR(255) NOT NULL ,
-  `product_group` VARCHAR(255) NOT NULL ,
-  `created_time` DATETIME NOT NULL ,
-  `updated_time` DATETIME NOT NULL ,
-  UNIQUE INDEX `product_id_UNIQUE` (`client_product_id` ASC) ,
-  PRIMARY KEY (`client_product_id`) ,
-  INDEX `client_id_idx` (`client_id` ASC) ,
-  INDEX `product_id_idx` (`product_id` ASC) ,
-  CONSTRAINT `client_id`
-    FOREIGN KEY (`client_id` )
-    REFERENCES `mojo`.`client_details` (`client_id` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `product_id`
-    FOREIGN KEY (`product_id` )
-    REFERENCES `mojo`.`product_details` (`product_id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `client_details` (
+  `client_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `created_time` int(11) NOT NULL,
+  `updated_time` int(11) NOT NULL,
+  PRIMARY KEY (`client_id`),
+  UNIQUE KEY `client_id_UNIQUE` (`client_id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `client_product`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE IF NOT EXISTS `client_product` (
+  `client_product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `client_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_group` varchar(255) NOT NULL,
+  `created_time` int(11) NOT NULL,
+  `updated_time` int(11) NOT NULL,
+  PRIMARY KEY (`client_product_id`),
+  UNIQUE KEY `product_id_UNIQUE` (`client_product_id`),
+  KEY `client_id_idx` (`client_id`),
+  KEY `product_id_idx` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_details`
+--
+
+CREATE TABLE IF NOT EXISTS `product_details` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(255) NOT NULL,
+  `product_group` varchar(255) NOT NULL,
+  `product_vendor` varchar(255) NOT NULL,
+  `created_time` int(11) NOT NULL,
+  `updated_time` int(11) NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `client_product`
+--
+ALTER TABLE `client_product`
+  ADD CONSTRAINT `client_id` FOREIGN KEY (`client_id`) REFERENCES `client_details` (`client_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `product_details` (`product_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
